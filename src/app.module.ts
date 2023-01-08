5ff75e2d1d7fa67473e70bb3f71150471c2ac39b
Ricardo Velez <ricardovelecal@gmail.com>
2023-01-07 18:17:02 -0600

    starting base auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { AuthsModule } from './auths/auths.module';
 
 
 @Module({
@@ -18,6 +19,7 @@ import { TypeOrmModule } from "@nestjs/typeorm";
       synchronize: true
     }),
     UsersModule,
+    AuthsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
