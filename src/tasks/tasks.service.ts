ced6da659f3e895859c07543876b5aa798d8a5b5
Ricardo Velez <ricardovelecal@gmail.com>
2023-01-09 09:17:47 -0600

    main: base tasks added v2

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UsersService } from '../users/users.service';
+import { Task } from './entity/tasks.entity';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+
+    constructor(
+        @InjectRepository(Task) private taskRepository:
+        Repository<Task>,
+        private usersService: UsersService
+    ){}
+
+    createTask() {
+
+    }
+
+    getTask() {
+        
+    }
+
+}
